661704d506be1cebc33cbd3c3f93e3739033170f
Daniel Vilela <danielvilelayo@gmail.com>
2025-03-21 01:17:32 -0500

    Add first implementation of big number wordinator

--- chapter_3/wordinator.c
+++ chapter_3/wordinator.c
@@ -1,6 +1,7 @@
 #include <alloca.h>
 #include <errno.h>
 #include <limits.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,6 +17,9 @@ typedef struct {
 } string;
 
 int parse_int_string(char *str, int *number);
+int wordinator_str_size(int number);
+void write_wordinator_str(int number, char *buffer, int *buffer_idx,
+                          int *capacity);
 
 string number_names[91] = {
     {.length = 4, .value = "zero"},     {.length = 3, .value = "one"},
@@ -30,17 +34,31 @@ string number_names[91] = {
     {.length = 8, .value = "eighteen"}, {.length = 8, .value = "nineteen"},
     {.length = 6, .value = "twenty"}};
 
-string bin_strings[4] = {{.length = 0, .value = ""},
+string bin_strings[4] = {{.length = 0, .value = NULL},
                          {.length = 9, .value = " thousand"},
                          {.length = 8, .value = " million"},
                          {.length = 8, .value = " billion"}};
 
 int main(int argc, char *argv[]) {
+  number_names[30] = (string){.length = 6, .value = "thirty"};
+  number_names[40] = (string){.length = 5, .value = "forty"};
   number_names[50] = (string){.length = 7, .value = "fifthty"};
   number_names[60] = (string){.length = 5, .value = "sixty"};
   number_names[70] = (string){.length = 7, .value = "seventy"};
   number_names[80] = (string){.length = 6, .value = "eighty"};
   number_names[90] = (string){.length = 6, .value = "ninety"};
+
+  int parsed_number;
+  int parse_status;
+  char *bin;
+
+  char *string_buffer;
+
+  int buffer_written;
+  int string_capacity;
+  int string_buffer_index;
+
+  int string_buffer_size = 1;
 
   if (argc < 2) {
     fprintf(stderr, "You must pass a number to wordinate\n");
@@ -58,8 +76,7 @@ int main(int argc, char *argv[]) {
   }
 
   if (number_str_length <= 3) {
-    int number;
-    int parse_status = parse_int_string(number_str, &number);
+    parse_status = parse_int_string(number_str, &parsed_number);
 
     if (parse_status == PARSE_FAILED) {
       fprintf(stderr, "Pliss use a numeric value\n");
@@ -68,10 +85,7 @@ int main(int argc, char *argv[]) {
       return 1;
     }
 
-    char *string_buffer;
-    int string_buffer_size = 1;
-
-    if (number == 0) {
+    if (parsed_number == 0) {
       string_buffer_size += number_names[0].length;
       string_buffer = (char *)alloca(string_buffer_size);
 
@@ -81,148 +95,93 @@ int main(int argc, char *argv[]) {
       return 0;
     }
 
-    int dec_number = number % 100;
-    int cent_part = (number - dec_number) / 100;
-
-    int base_part;
-    int dec_part;
-
-    if (cent_part > 0) {
-      string_buffer_size += number_names[cent_part].length + 8;
-
-      if (dec_number > 0) {
-        string_buffer_size += 5;
-      }
-    }
-
-    if (dec_number > 0 && dec_number <= 20) {
-      string_buffer_size += number_names[dec_number].length;
-    } else if (dec_number > 0) {
-      base_part = dec_number % 10;
-      dec_part = (dec_number - base_part);
-
-      string_buffer_size +=
-          number_names[dec_part].length + number_names[base_part].length + 1;
-    }
+    string_buffer_size += wordinator_str_size(parsed_number);
 
     string_buffer = (char *)alloca(string_buffer_size);
-    int string_capacity = string_buffer_size;
-    int buffer_index = 0;
+    string_capacity = string_buffer_size;
 
-    int buffer_written;
-
-    if (cent_part > 0) {
-      buffer_written = snprintf(&string_buffer[buffer_index], string_capacity,
-                                "%s hundred", number_names[cent_part].value);
-
-      buffer_index += buffer_written;
-      string_capacity -= buffer_written;
-
-      if (dec_number > 0) {
-        buffer_written = snprintf(&string_buffer[buffer_index], string_capacity,
-                                  "%s", " and ");
-
-        buffer_index += buffer_written;
-        string_capacity -= buffer_written;
-      }
-    }
-
-    if (dec_number > 0 && dec_number <= 20) {
-      buffer_written = snprintf(&string_buffer[buffer_index], string_capacity,
-                                "%s", number_names[dec_number].value);
-
-      buffer_index += buffer_written;
-      string_capacity -= buffer_written;
-    } else {
-      buffer_written =
-          snprintf(&string_buffer[buffer_index], string_capacity, "%s-%s",
-                   number_names[dec_part].value, number_names[base_part].value);
-
-      buffer_index += buffer_written;
-      string_capacity -= buffer_written;
-    }
+    write_wordinator_str(parsed_number, string_buffer, &string_buffer_index,
+                         &string_capacity);
 
     printf("The number is %s\n", string_buffer);
 
     return 0;
   }
 
-  short tail_number = number_str_length % 3;
+  char buffer[4];
+  int buffer_idx = 0;
 
-  number_str_length -= tail_number;
+  int parsed_numbers_length = ceil(number_str_length / 3.);
+  int parsed_numbers[parsed_numbers_length];
+  int parsed_numbers_idx = 0;
 
-  int bins = number_str_length / 3;
+  int i = 0;
+  while (i <= number_str_length) {
+    if (((number_str_length - i) % 3) != 0 || buffer_idx == 0) {
+      snprintf(&buffer[buffer_idx], 4, "%c", number_str[i]);
 
-  int number;
-  int parse_status = parse_int_string(number_str, &number);
+      buffer_idx++;
+      i++;
 
-  if (parse_status == PARSE_FAILED) {
-    fprintf(stderr, "Pliss use a numeric value\n");
-    PRINT_USAGE(argv[0]);
-
-    return 1;
-  }
-
-  int string_buffer_size = 1;
-
-  int dec_number = number % 100;
-  int cent_part = (number - dec_number) / 100;
-
-  int base_part;
-  int dec_part;
-
-  if (cent_part > 0) {
-    string_buffer_size += number_names[cent_part].length + 8;
-
-    if (dec_number > 0) {
-      string_buffer_size += 5;
+      continue;
     }
-  }
 
-  if (dec_number > 0 && dec_number <= 20) {
-    string_buffer_size += number_names[dec_number].length;
-  } else if (dec_number > 0) {
-    base_part = dec_number % 10;
-    dec_part = (dec_number - base_part);
+    parse_status = parse_int_string(number_str, &parsed_number);
+    if (parse_status == PARSE_FAILED) {
+      fprintf(stderr, "Pliss use a numeric value\n");
+      PRINT_USAGE(argv[0]);
+
+      return 1;
+    }
+
+    parsed_numbers[parsed_numbers_idx] = parsed_number;
+    parsed_numbers_idx++;
+
+    buffer_idx = 0;
+    if (parsed_number == 0) {
+      continue;
+    }
+
+    if (string_buffer_size > 1) {
+      string_buffer_size += 2;
+    }
 
     string_buffer_size +=
-        number_names[dec_part].length + number_names[base_part].length + 1;
+        wordinator_str_size(parsed_number) + bin_strings[i / 3].length;
   }
 
-  char string_buffer[string_buffer_size];
-  int string_capacity = string_buffer_size;
-  int buffer_index = 0;
+  string_buffer = alloca(string_buffer_size);
+  string_capacity = string_buffer_size;
+  string_buffer_index = 0;
 
-  int buffer_written;
+  for (i = 0; i < parsed_numbers_length; i++) {
+    parsed_number = parsed_numbers[i];
 
-  if (cent_part > 0) {
-    buffer_written = snprintf(&string_buffer[buffer_index], string_capacity,
-                              "%s hundred", number_names[cent_part].value);
+    if (parsed_number == 0) {
+      continue;
+      ;
+    }
 
-    buffer_index += buffer_written;
-    string_capacity -= buffer_written;
+    if (buffer_written > 0) {
+      buffer_written = snprintf(&string_buffer[string_buffer_index],
+                                string_capacity, "%s", ", ");
 
-    if (dec_number > 0) {
-      buffer_written = snprintf(&string_buffer[buffer_index], string_capacity,
-                                "%s", " and ");
-
-      buffer_index += buffer_written;
+      string_buffer_index += buffer_written;
       string_capacity -= buffer_written;
     }
-  }
 
-  if (dec_number > 0 && dec_number <= 20) {
-    buffer_written = snprintf(&string_buffer[buffer_index], string_capacity,
-                              "%s", number_names[dec_number].value);
+    write_wordinator_str(parsed_number, string_buffer, &string_buffer_index,
+                         &string_capacity);
 
-    buffer_index += buffer_written;
-    string_capacity -= buffer_written;
-  } else {
-    buffer_written =
-        snprintf(&string_buffer[buffer_index], string_capacity, "%s-%s",
-                 number_names[dec_part].value, number_names[base_part].value);
+    bin = bin_strings[parsed_numbers_length - (i + 1)].value;
+    if (bin == NULL) {
+      continue;
+    }
 
-    buffer_index += buffer_written;
+    buffer_written = snprintf(&string_buffer[string_buffer_index],
+                              string_capacity, "%s", bin);
+
+    string_buffer_index += buffer_written;
     string_capacity -= buffer_written;
   }
 
@@ -250,4 +209,83 @@ int parse_int_string(char *str, int *number) {
   }
 
   return PARSE_SUCCESS;
+}
+
+int wordinator_str_size(int number) {
+  int size = 0;
+
+  if (number == 0) {
+    size += number_names[0].length;
+
+    return size;
+  }
+
+  int dec_number = number % 100;
+  int cent_part = (number - dec_number) / 100;
+
+  int base_part;
+  int dec_part;
+
+  if (cent_part > 0) {
+    size += number_names[cent_part].length + 8;
+
+    if (dec_number > 0) {
+      size += 5;
+    }
+  }
+
+  if (dec_number > 0 && dec_number <= 20) {
+    size += number_names[dec_number].length;
+  } else if (dec_number > 0) {
+    base_part = dec_number % 10;
+    dec_part = (dec_number - base_part);
+
+    size += number_names[dec_part].length + number_names[base_part].length + 1;
+  }
+
+  return size;
+}
+
+void write_wordinator_str(int number, char *buffer, int *buffer_idx,
+                          int *capacity) {
+  int dec_number = number % 100;
+  int cent_part = (number - dec_number) / 100;
+
+  int base_part;
+  int dec_part;
+
+  int written;
+
+  if (cent_part > 0) {
+    written = snprintf(&buffer[*buffer_idx], *capacity, "%s hundred",
+                       number_names[cent_part].value);
+
+    *buffer_idx += written;
+    *capacity -= written;
+
+    if (dec_number > 0) {
+      written = snprintf(&buffer[*buffer_idx], *capacity, "%s", " and ");
+
+      *buffer_idx += written;
+      *capacity -= written;
+    }
+  }
+
+  if (dec_number > 0 && dec_number <= 20) {
+    written = snprintf(&buffer[*buffer_idx], *capacity, "%s",
+                       number_names[dec_number].value);
+
+    *buffer_idx += written;
+    *capacity -= written;
+  } else if (dec_number > 0) {
+    base_part = dec_number % 10;
+    dec_part = (dec_number - base_part);
+
+    written =
+        snprintf(&buffer[*buffer_idx], *capacity, "%s-%s",
+                 number_names[dec_part].value, number_names[base_part].value);
+
+    *buffer_idx += written;
+    *capacity -= written;
+  }
 }
